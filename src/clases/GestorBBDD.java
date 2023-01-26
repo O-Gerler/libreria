6c8dff7d238e007f363c26d9795276dee61e22e3
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 22:58:52 +0100

    metodo getLibro

--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -1,6 +1,7 @@
 package clases;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class GestorBBDD extends Conector {
 
@@ -26,5 +27,26 @@ public class GestorBBDD extends Conector {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public Libro getLibro(int id) {
+		
+		Libro libro = null;
+		
+		try {
+			PreparedStatement pst = super.connection
+					.prepareStatement("SELECT FROM libros WHERE id=?");
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+			libro = new Libro();
+			libro.setId(rs.getInt("id"));
+			libro.setTitulo(rs.getString("titulo"));
+			libro.setAutor(rs.getString("autor"));
+			libro.setNumPaginas(rs.getInt("num_pag"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return libro;
 	}
 }
