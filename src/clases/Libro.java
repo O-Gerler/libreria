c1c3544f3929e4e0c1a40b27227605f58b8942c5
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 15:59:33 +0100

    setters y getters

--- src/clases/Libro.java
+++ src/clases/Libro.java
@@ -1,5 +1,35 @@
 package clases;
 
 public class Libro {
-
+	private int id;
+	private String titulo;
+	private String autor;
+	private int numPaginas;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getNumPaginas() {
+		return numPaginas;
+	}
+	public void setNumPaginas(int numPaginas) {
+		this.numPaginas = numPaginas;
+	}
+	
+	
 }
