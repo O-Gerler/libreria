633730cb97b9230ecf70a0d00f2ee0582a3c05bf
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 16:01:11 +0100

    setters y getters

--- src/clases/Conector.java
+++ src/clases/Conector.java
@@ -1,5 +1,19 @@
 package clases;
 
+import java.sql.Connection;
+
 public class Conector {
+	
+	private Connection connection;
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	
+	
 
 }
