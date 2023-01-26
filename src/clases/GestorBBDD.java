97a365dfaf243e106f807fbb09b29ecf09cb8d6b
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 16:33:39 +0100

    metodo insertar libro agregado

--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -1,5 +1,20 @@
 package clases;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
 
+public class GestorBBDD extends Conector{
+	
+	public void insertarLibro(Libro libro) {
+		try {
+			PreparedStatement pst = super.connection
+					.prepareStatement("INSERT INTO libros (titulo,autor,num_pag)"
+							+ "VALUES (?,?,?)");
+			pst.setString(1, libro.getTitulo());
+			pst.setString(2, libro.getAutor());
+			pst.setInt(3, libro.getNumPaginas());
+			pst.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
