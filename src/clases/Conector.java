ad990d5af89bb3653ffd98d458dfa955855894a7
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 16:06:07 +0100

    metodo conectar y cerrar

--- src/clases/Conector.java
+++ src/clases/Conector.java
@@ -1,6 +1,8 @@
 package clases;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conector {
 	
@@ -14,6 +16,25 @@ public class Conector {
 		this.connection = connection;
 	}
 	
+	public void conectar() {
+		final String HOST = "jdbc:mysql://localhost/biblioteca2";
+		final String USER = "root";
+		final String PASSWORD = "";
+		
+		try {
+			Class.forName("com.jdbc.mysql.Driver");
+			connection = DriverManager.getConnection(HOST,USER,PASSWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
+	public void cerrar() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
