df87e538a518e703fc672bffffd357c40f20ebd0
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 16:10:11 +0100

    metodo mostrarMenuPrincipal

--- src/clases/Menu.java
+++ src/clases/Menu.java
@@ -1,5 +1,14 @@
 package clases;
 
 public class Menu {
-
+	public static void mostrarMenuPrincipal() {
+		final int GESTIONAR_LIBROS = 1;
+		final int GESTIONAR_SOCIOS = 1;
+		final int GESTIONAR_PRESTAMOS = 1;
+		
+		System.out.println("---------------MENU-PRINCIPAL---------------");
+		System.out.println(GESTIONAR_LIBROS  + ".- Gestionar libros");
+		System.out.println(GESTIONAR_SOCIOS  + ".- Gestionar socios");
+		System.out.println(GESTIONAR_PRESTAMOS  + ".- Gestionar prestamos");
+	}
 }
