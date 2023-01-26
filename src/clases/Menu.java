6c53fdbcafa7b8bfe871fceae87d5845971d17d0
azurm <azurm@DESKTOP-4CVAMG8>
2023-01-26 16:12:53 +0100

    metodo mostrarMenuPrincipal ahora los numero estan bien puestos

--- src/clases/Menu.java
+++ src/clases/Menu.java
@@ -3,8 +3,8 @@ package clases;
 public class Menu {
 	public static void mostrarMenuPrincipal() {
 		final int GESTIONAR_LIBROS = 1;
-		final int GESTIONAR_SOCIOS = 1;
-		final int GESTIONAR_PRESTAMOS = 1;
+		final int GESTIONAR_SOCIOS = 2;
+		final int GESTIONAR_PRESTAMOS = 3;
 		
 		System.out.println("---------------MENU-PRINCIPAL---------------");
 		System.out.println(GESTIONAR_LIBROS  + ".- Gestionar libros");
