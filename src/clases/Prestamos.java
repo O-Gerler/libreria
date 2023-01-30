6aa0b1005922812072a8163a196251f5866082b3
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-01-30 13:20:30 +0100

    atributos getters y setters

--- src/clases/Prestamos.java
+++ src/clases/Prestamos.java
@@ -1,5 +1,37 @@
 package clases;
 
-public class Prestamos {
+import java.util.Date;
 
+public class Prestamos {
+	private int idLibro;
+	private int idSocio;
+	private Date fecha;
+	private boolean devuelto;
+	
+	public int getIdLibro() {
+		return idLibro;
+	}
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+	public int getIdSocio() {
+		return idSocio;
+	}
+	public void setIdSocio(int idSocio) {
+		this.idSocio = idSocio;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isDevuelto() {
+		return devuelto;
+	}
+	public void setDevuelto(boolean devuelto) {
+		this.devuelto = devuelto;
+	}
+	
+	
 }
