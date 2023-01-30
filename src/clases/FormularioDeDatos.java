9eab06a65f2b19cc752a24ebd57e00178c1e54c1
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-01-30 13:44:22 +0100

    metodo pedirDatosLibro

--- src/clases/FormularioDeDatos.java
+++ src/clases/FormularioDeDatos.java
@@ -1,5 +1,22 @@
 package clases;
 
+import java.util.Scanner;
+
 public class FormularioDeDatos {
 
+	public static Libro pedirDatosLibro(Scanner scan) {
+		System.out.println("Introduce el titulo de el libro");
+		String titulo = scan.nextLine();
+		System.out.println("Introduce el autor de el libro");
+		String autor = scan.nextLine();
+		System.out.println("Introduce el numero de paginas del libro");
+		int numPaginas = Integer.parseInt(scan.nextLine());
+		
+		Libro libro = new Libro();
+		libro.setTitulo(titulo);
+		libro.setAutor(autor);
+		libro.setNumPaginas(numPaginas);
+		
+		return libro;
+	}
 }
