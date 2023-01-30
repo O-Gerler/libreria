6eff4c5afd4c164a10e17eb0748d6abfe3b19451
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-01-30 13:55:58 +0100

    metodos pedirIDLibro y modificarDatosLibro

--- src/clases/FormularioDeDatos.java
+++ src/clases/FormularioDeDatos.java
@@ -3,14 +3,29 @@ package clases;
 import java.util.Scanner;
 
 public class FormularioDeDatos {
-
-	public static Libro pedirDatosLibro(Scanner scan) {
-		System.out.println("Introduce el titulo de el libro");
-		String titulo = scan.nextLine();
-		System.out.println("Introduce el autor de el libro");
-		String autor = scan.nextLine();
+	
+	private static int pedirNumPaginasLibro(Scanner scan) {
 		System.out.println("Introduce el numero de paginas del libro");
 		int numPaginas = Integer.parseInt(scan.nextLine());
+		return numPaginas;
+	}
+
+	private static String pedirAutorLibro(Scanner scan) {
+		System.out.println("Introduce el autor de el libro");
+		String autor = scan.nextLine();
+		return autor;
+	}
+
+	private static String pedirTituloLibro(Scanner scan) {
+		System.out.println("Introduce el titulo de el libro");
+		String titulo = scan.nextLine();
+		return titulo;
+	}
+
+	public static Libro pedirDatosLibro(Scanner scan) {
+		String titulo = pedirTituloLibro(scan);
+		String autor = pedirAutorLibro(scan);
+		int numPaginas = pedirNumPaginasLibro(scan);
 		
 		Libro libro = new Libro();
 		libro.setTitulo(titulo);
@@ -18,5 +33,27 @@ public class FormularioDeDatos {
 		libro.setNumPaginas(numPaginas);
 		
 		return libro;
+	}
+
+	public static Libro modificarDatosLibro(Libro libro, Scanner scan) {
+		System.out.println("Datos actuales:");
+		System.out.println(libro);
+		
+		String titulo = pedirTituloLibro(scan);
+		String autor = pedirAutorLibro(scan);
+		int numPaginas = pedirNumPaginasLibro(scan);
+		
+		libro.setTitulo(titulo);
+		libro.setAutor(autor);
+		libro.setNumPaginas(numPaginas);
+		
+		return libro;
+	}
+	
+	public static int pedirIDLibro(Scanner scan) {
+		System.out.println("Introduce el ID del libro");
+		int id = Integer.parseInt(scan.nextLine());
+		
+		return id;
 	}
 }
