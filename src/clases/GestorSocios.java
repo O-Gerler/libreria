e7ccf5601cf95caa1e7bfe1d94de1d4717ed98f7
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-01-30 13:11:37 +0100

    menu gestionarSocios

--- src/clases/GestorSocios.java
+++ src/clases/GestorSocios.java
@@ -1,5 +1,37 @@
 package clases;
 
+import java.util.Scanner;
+
 public class GestorSocios {
 
+	public static void run(Scanner scan) {
+		int opcion;
+		do {
+			Menu.mostrarMenuSocios();
+			opcion = seleccionarOpcion(scan);
+			switch(opcion) {
+			case Menu.GESTIONAR_SOCIOS:
+				System.out.println("Gestionar Socios");
+				break;
+			case Menu.SOCIOS_SALIR:
+				System.out.println("Saliendo...");
+				break;
+			default:
+				System.out.println("Opcion invalida!");
+			}
+		} while (opcion == Menu.SOCIOS_SALIR);
+	}
+
+	private static int seleccionarOpcion(Scanner scan) {
+		int opcion;
+		
+		try {
+			System.out.print("Introduce la opcion: ");
+			opcion = Integer.parseInt(scan.nextLine());
+		} catch (Exception e) {
+			opcion = -1;
+		}
+		
+		return opcion;
+	}
 }
