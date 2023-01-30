7972e5d68d6b562a1b6e00e24efba0141f444bc3
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-01-30 13:33:58 +0100

    menu hecho

--- src/clases/GestorLibros.java
+++ src/clases/GestorLibros.java
@@ -1,5 +1,46 @@
 package clases;
 
+import java.util.Scanner;
+
 public class GestorLibros {
 
+	public void run(Scanner scan) {
+		int opcion;
+		
+		do {
+			Menu.mostrarMenuLibros();
+			opcion = seleccionarOpcion(scan);
+
+			switch (opcion) {
+			case Menu.INSERTAR_LIBRO:
+				System.out.println("Insertar libro");
+				break;
+			case Menu.ELIMINAR_LIBRO:
+				System.out.println("Eliminar libro");
+				break;
+			case Menu.VER_LIBROS:
+				System.out.println("Ver libros");
+				break;
+			case Menu.LIBROS_SALIR:
+				System.out.println("Saliendo...");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion != Menu.LIBROS_SALIR);
+	}
+	
+	private static int seleccionarOpcion(Scanner scan) {
+		int opcion;
+		
+		try {
+			System.out.print("Introduce la opcion: ");
+			opcion = Integer.parseInt(scan.nextLine());
+		} catch (Exception e) {
+			opcion = -1;
+		}
+		
+		return opcion;
+	}
 }
