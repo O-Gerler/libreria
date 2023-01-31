aaf53013bd93998a25436e797f7e0eaa1e590f8c
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-01-31 09:34:33 +0100

    metodo mostrar Socios y mostrarSocio

--- src/clases/Visor.java
+++ src/clases/Visor.java
@@ -1,6 +1,7 @@
 package clases;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Visor {
 	
@@ -12,6 +13,18 @@ public class Visor {
 	
 	public static void mostrarLibro(Libro libro) {
 		System.out.println(libro);
+	}
+	
+	/*-----------------------------SOCIOS-------------------------*/
+	
+	public static void mostrarSocios(ArrayList<Socio> socios){
+		for (Socio socio : socios) {
+			System.out.println(socio);
+		}
+	}
+	
+	public static void mostrarSocio(Socio socio) {
+		System.out.println(socio);
 	}
 	
 	public static void mostrarMensaje(String mensaje) {
