02f5d10e4875c37559d6991849a8cf04f4e268ab
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-02-02 12:09:11 +0100

    menu y opciones

--- src/clases/GestorPresatmos.java
+++ src/clases/GestorPresatmos.java
@@ -1,5 +1,50 @@
 package clases;
 
-public class GestorPresatmos {
+import java.util.Scanner;
 
+public class GestorPresatmos {
+	
+	public void run(Scanner scan) {
+		int opcion;
+		GestorBBDD gestorBBDD = new GestorBBDD();
+		
+		do {
+			Menu.mostrarMenuLibros();
+			opcion = seleccionarOpcion(scan);
+
+			switch (opcion) {
+			case Menu.REALIZAR_PRESTAMO:
+				System.out.println("Insertar prestamo");
+				break;
+			case Menu.ELIMINAR_PRESTAMO:
+				System.out.println("Eliminar prestamo");
+				break;
+			//case Menu.MODIFICAR_LIBRO:
+				/*no esta*/
+			//	break;
+			case Menu.VER_PRESTAMO:
+				System.out.println("Ver prestamos");
+				break;
+			case Menu.PRESTAMOS_SALIR:
+				System.out.println("Saliendo...");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion != Menu.PRESTAMOS_SALIR);
+	}
+	
+	private static int seleccionarOpcion(Scanner scan) {
+		int opcion;
+		
+		try {
+			System.out.print("Introduce la opcion: ");
+			opcion = Integer.parseInt(scan.nextLine());
+		} catch (Exception e) {
+			opcion = -1;
+		}
+		
+		return opcion;
+	}
 }
